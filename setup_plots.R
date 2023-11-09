4f6fba94e1df281b7b2e0a433138c3cbd30bbe68
Kevin <likevin@umich.edu>
2023-11-08 22:54:23 -0500

    add 2023
    add 2023 calculations

---
+++ setup_plots.R
@@ -0,0 +1,17 @@
+library(sf)
+library(tidyverse)
+
+plots10_22 <- st_read("./data/GIS/plots10_2022.geojson") %>% mutate(plot_num =1)
+plots10_23 <- st_read("./data/GIS/plots10_2023.geojson")
+
+plots10 <- bind_rows(plots10_22, plots10_23) %>% 
+  mutate(Plot = paste0("Plot ", plot_num),
+         Size = "10x10")
+
+plots30 <- st_read("./data/GIS/plots30_2023.geojson") %>%
+  mutate(Plot = paste0("Plot ", plot_num),
+         Size = "30x30")
+
+# write out combined data
+st_write(plots10, "./data/GIS/plots10.geojson")
+st_write(plots30, "./data/GIS/plots30.geojson")
